4c8078600a7891c5e71eb4dd0bbcaadd8bcdf199
Proton <feisuzhu@163.com>
2020-02-04 16:02:13 +0800

    :tada: Initial commit, basically working

---
+++ src/check.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, annotations, division, print_function, unicode_literals
+
+# -- stdlib --
+import types
+
+# -- third party --
+# -- own --
+
+# -- code --
+
+
+class CheckTypeFailed(Exception):
+    def __init__(self):
+        Exception.__init__(self)
+        self.path = []
+
+    def path_string(self):
+        return ''.join([
+            self._path_fragment(v)
+            for v in self.path
+        ])
+
+    def _path_fragment(self, v):
+        if isinstance(v, int):
+            return '[%s]' % v
+        elif isinstance(v, str):
+            return '.%s' % v
+
+    def finalize(self):
+        self.args = (self.path_string(), )
+
+
+def _check(cond):
+    if not cond:
+        raise CheckTypeFailed
+
+
+def _check_isinstance(obj, cls):
+    try:
+        _check(isinstance(obj, cls))
+    except TypeError as e:
+        raise CheckTypeFailed from e
+
+
+_check_key_not_exists = object()
+
+
+def check_type_exc(pattern, obj, path=None):
+    try:
+        if isinstance(pattern, (list, tuple)):
+            _check_isinstance(obj, (list, tuple))
+            if len(pattern) == 2 and pattern[-1] is ...:
+                cls = pattern[0]
+                for i, v in enumerate(obj):
+                    check_type_exc(cls, v, i)
+            else:
+                _check(len(pattern) == len(obj))
+                for i, (cls, v) in enumerate(zip(pattern, obj)):
+                    check_type_exc(cls, v, i)
+
+        elif isinstance(pattern, dict):
+            _check_isinstance(obj, dict)
+            if ... in pattern:
+                pattern = dict(pattern)
+                match = pattern.pop(...)
+            else:
+                match = '!'
+
+            if match in set('?!='):
+                lkeys = set(pattern.keys())
+                rkeys = set(obj.keys())
+
+                if match == '!':
+                    iterkeys = lkeys
+                elif match == '?':
+                    iterkeys = lkeys & rkeys
+                elif match == '=':
+                    _check(lkeys == rkeys)
+                    iterkeys = lkeys
+                else:
+                    assert False, 'WTF?!'
+
+                for k in iterkeys:
+                    check_type_exc(pattern[k], obj.get(k, _check_key_not_exists), k)
+
+            elif match is ...:
+                assert len(pattern) == 1, 'Invalid dict pattern'
+                kt, vt = list(pattern.items())[0]
+                for k in obj:
+                    check_type_exc(kt, k, '<%s>' % kt.__name__)
+                    check_type_exc(vt, obj[k], k)
+            else:
+                assert False, 'Invalid dict match type'
+
+        else:
+            if issubclass(type(pattern), types.FunctionType):
+                try:
+                    _check(pattern(obj))
+                except Exception as e:
+                    raise CheckTypeFailed from e
+            elif issubclass(type(pattern), (int, str, bytes, tuple)):
+                _check(obj == pattern)
+            else:
+                _check_isinstance(obj, pattern)
+
+    except CheckTypeFailed as e:
+        if path is not None:
+            e.path.insert(0, path)
+        else:
+            e.finalize()
+
+        raise
+
+
+def check_type(pattern, obj):
+    try:
+        check_type_exc(pattern, obj)
+        return None
+    except CheckTypeFailed as e:
+        return e.path_string()
---
+++ src/serve.py
@@ -0,0 +1,284 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+# -- stdlib --
+import argparse
+import base64
+import logging
+import functools
+import operator
+import re
+
+# -- third party --
+from flask import Flask, request, jsonify
+import bjoern
+import msgpack
+import numpy as np
+import onnxruntime as rt
+
+# -- own --
+from check import check_type
+
+
+# -- code --
+log = logging.getLogger('onnxrt-server')
+app = Flask('onnxrt-server')
+model = None
+
+
+# https://github.com/microsoft/onnxruntime/blob/v0.4.0/onnxruntime/python/onnxruntime_pybind_mlvalue.cc
+ONNX_TO_NP_TYPE = {
+    "tensor(float16)": "float16",
+    "tensor(float)":   "float32",
+    "tensor(double)":  "float64",
+    "tensor(int32)":   "int32",
+    "tensor(uint32)":  "uint32",
+    "tensor(int8)":    "int8",
+    "tensor(uint8)":   "uint8",
+    "tensor(int16)":   "int16",
+    "tensor(uint16)":  "uint16",
+    "tensor(int64)":   "int64",
+    "tensor(uint64)":  "uint64",
+    "tensor(bool)":    "bool",
+    "tensor(string)":  "object",
+}
+
+
+class AppError(Exception):
+
+    def __init__(self, message, status_code, payload={}):
+        Exception.__init__(self)
+        self.message = message
+        self.status_code = status_code
+        self.payload = {**payload, 'error': message}
+
+
+
+@app.errorhandler(AppError)
+def handle_app_error(e):
+    response = jsonify(e.payload)
+    response.status_code = e.status_code
+    return response
+
+
+@app.errorhandler(Exception)
+def report_exception(e):
+    log.exception('Catched exception')
+    return {'error': str(e)}, 500
+
+
+@app.route('/v1/models/default')
+def model_status():
+    return 'not impl'
+
+
+@app.route('/v1/models/default/metadata')
+def model_metadata():
+    inputs = [{
+        'name':  i.name,
+        'shape': i.shape,
+        'type':  i.type,
+    } for i in model.get_inputs()]
+
+    outputs = [{
+        'name':  i.name,
+        'shape': i.shape,
+        'type':  i.type,
+    } for i in model.get_outputs()]
+
+    meta = model.get_modelmeta()
+    meta = {
+        'custom_metadata_map': meta.custom_metadata_map,
+        'description':         meta.description,
+        'domain':              meta.domain,
+        'graph_name':          meta.graph_name,
+        'producer_name':       meta.producer_name,
+        'version':             str(meta.version),
+    }
+
+    return {
+        'inputs': inputs,
+        'outputs': outputs,
+        'metadata': meta,
+    }
+
+
+def parse_type(tp):
+    if not tp or tp == '*/*':
+        return ('base64', 'json')
+
+    m = re.match(r'^application/(?:([a-z0-9]+)\+)?([a-z0-9\.\-]+)(?:[, ;]|$)', tp)
+    if not m:
+        return None
+
+    fmt, enc = m.groups()
+
+    COALESCE = {
+        'msgpack':         'msgpack',
+        'x-msgpack':       'msgpack',
+        'vnd.msgpack':     'msgpack',
+        'vnd.messagepack': 'msgpack',
+        'json':            'json',
+    }
+
+    if enc not in COALESCE:
+        return None
+
+    enc = COALESCE[enc]
+
+    DEFAULT_FMT = {
+        'msgpack': 'bytes',
+        'json':    'base64',
+    }
+
+    fmt = fmt or DEFAULT_FMT.get(enc)
+
+    if fmt not in ('bytes', 'base64', 'plain'):
+        return None
+
+    COMPATIBLE  = (
+        ('base64', 'json'),
+        ('plain',  'json'),
+
+        ('bytes',  'msgpack'),
+        ('base64', 'msgpack'),
+        ('plain',  'msgpack'),
+    )
+
+    if (fmt, enc) not in COMPATIBLE:
+        return None
+
+    return (fmt, enc)
+
+
+def get_payload():
+    tp = parse_type(request.mimetype)
+    if not tp:
+        raise AppError("Invalid Content-Type", 415)
+
+    fmt, enc = tp
+    if enc == 'msgpack':
+        req = msgpack.loads(request.data)
+    elif enc == 'json':
+        req = request.json
+    else:
+        raise Exception('WTF')
+
+    return fmt, req
+
+
+def decode_input(model_input, data, fmt):
+    assert fmt in ('plain', 'base64', 'bytes')
+    i = model_input
+    tp = getattr(np, ONNX_TO_NP_TYPE[i.type])
+
+    if fmt == 'plain':
+        a = np.asarray(data, dtype=tp)
+        if a.shape != i.shape:
+            raise AppError(f'Input "{i.name}" shape mismatch: provided({list(a.shape)}) != expected({list(i.shape)})', 400)
+        return a
+
+    if fmt == 'base64':
+        d = base64.b64decode(data)
+    elif fmt == 'bytes':
+        if not isinstance(data, bytes):
+            raise AppError(f'Input "{i.name}" is not bytes')
+        d = data
+
+    a = np.frombuffer(d, dtype=tp)
+    expected_size = functools.reduce(operator.mul, i.shape, a.itemsize)
+    if a.nbytes != expected_size:
+        raise AppError(f'Input "{i.name}" size mismatch: provided({a.nbytes} bytes) != expected({expected_size} bytes)', 400)
+    a = a.reshape(*i.shape)
+    return a
+
+
+def make_output(results):
+    tp = parse_type(request.headers['Accept'])
+    if not tp:
+        raise AppError("Invalid Accept", 406)
+
+    fmt, enc = tp
+    rst = {}
+    for k, v in results.items():
+        if fmt == 'bytes':
+            rst[k] = v.tobytes()
+        elif fmt == 'base64':
+            rst[k] = base64.b64encode(v.tobytes()).decode('utf-8')
+        elif fmt == 'plain':
+            rst[k] = v.tolist()
+        else:
+            raise Exception('WTF')
+
+    if enc == 'msgpack':
+        return msgpack.dumps(rst), 200, {'Content-Type': 'application/msgpack'}
+    elif enc == 'json':
+        return rst
+    else:
+        raise Exception('WTF')
+
+
+@app.route('/v1/models/default:predict', methods=['POST'])
+def model_predict():
+    r'''
+    {
+        "inputs": {
+            "input.1": "AAAA",
+            "input.1": "\x00\x00\x00",
+            "input.1": [[1,2,3], [4,5,6]],
+        },
+        "outputs": ["output.1"]
+    }
+    '''
+    fmt, req = get_payload()
+
+    err = check_type({
+        'inputs': {
+            str: object,
+            ...: ...,
+        },
+        'outputs': [str, ...],
+    }, req)
+
+    if err:
+        return {'error': f"Bad input: {err}"}, 400
+
+    inputs = req['inputs']
+    outputs = req['outputs']
+    if not outputs:
+        return {'error': f"No output selected"}, 400
+
+    unknown_outputs = set(outputs) - set(i.name for i in model.get_outputs())
+    if unknown_outputs:
+        return {'error': f"Unknown outputs: {' '.join(unknown_outputs)}"}, 400
+
+    np_inputs = {}
+    for i in model.get_inputs():
+        if i.name not in inputs:
+            return {'error': f"Input '{i.name}' not present"}, 400
+        np_inputs[i.name] = decode_input(i, inputs[i.name], fmt)
+
+    pred = model.run(outputs, np_inputs)
+    results = dict(zip(outputs, pred))
+
+    return make_output(results)
+
+
+def load_model(path):
+    global model
+    model = rt.InferenceSession(path)
+
+
+def main():
+    parser = argparse.ArgumentParser('onnxrt-server')
+    parser.add_argument('--model', default='/models/model.onnx')
+    parser.add_argument('--host', default='0.0.0.0')
+    parser.add_argument('--port', type=int, default=8001)
+    options = parser.parse_args()
+
+    load_model(options.model)
+    bjoern.run(app, options.host, options.port)
+
+
+if __name__ == '__main__':
+    main()
